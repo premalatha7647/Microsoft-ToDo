e10a42a1ee9cfd18c713ac7fd6fa9b550b74fd85
premalatha <169128822+premalatha8647@users.noreply.github.com>
2024-05-16 22:56:22 +0530

    Create AntdDrawer.vue

---
+++ src/components/ReusableComponents/AntdDrawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-button type="primary" @click="showDrawer">Open</a-button>
+  <a-drawer v-model:open="open" :get-container="false" class="custom-class" root-class-name="root-class-name"
+    :root-style="{ color: 'blue' }" style="color: red" title="Basic Drawer" placement="right"
+    @after-open-change="afterOpenChange">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-drawer>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+const open = ref<boolean>(false);
+
+const afterOpenChange = (bool: boolean) => {
+  console.log('open', bool);
+};
+
+const showDrawer = () => {
+  open.value = true;
+};
+</script>
