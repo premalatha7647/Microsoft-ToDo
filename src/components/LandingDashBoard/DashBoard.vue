f3d0aa1cd10411b0f53644d66842dadaa6f1d05f
premalatha <169128822+premalatha8647@users.noreply.github.com>
2024-05-16 22:56:41 +0530

    Task container created

--- src/components/LandingDashBoard/DashBoard.vue
+++ src/components/LandingDashBoard/DashBoard.vue
@@ -4,55 +4,10 @@
             <DashBoardHeader></DashBoardHeader>
         </a-layout-header>
         <a-layout>
-            <a-layout-sider width="200" style="background: #fff">
-                <a-menu v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" mode="inline"
-                    :style="{ height: '100%', borderRight: 0 }">
-                    <a-sub-menu key="sub1">
-                        <template #title>
-                            <span>
-                                <user-outlined />
-                                subnav 1
-                            </span>
-                        </template>
-                        <a-menu-item key="1">option1</a-menu-item>
-                        <a-menu-item key="2">option2</a-menu-item>
-                        <a-menu-item key="3">option3</a-menu-item>
-                        <a-menu-item key="4">option4</a-menu-item>
-                    </a-sub-menu>
-                    <a-sub-menu key="sub2">
-                        <template #title>
-                            <span>
-                                <laptop-outlined />
-                                subnav 2
-                            </span>
-                        </template>
-                        <a-menu-item key="5">option5</a-menu-item>
-                        <a-menu-item key="6">option6</a-menu-item>
-                        <a-menu-item key="7">option7</a-menu-item>
-                        <a-menu-item key="8">option8</a-menu-item>
-                    </a-sub-menu>
-                    <a-sub-menu key="sub3">
-                        <template #title>
-                            <span>
-                                <notification-outlined />
-                                subnav 3
-                            </span>
-                        </template>
-                        <a-menu-item key="9">option9</a-menu-item>
-                        <a-menu-item key="10">option10</a-menu-item>
-                        <a-menu-item key="11">option11</a-menu-item>
-                        <a-menu-item key="12">option12</a-menu-item>
-                    </a-sub-menu>
-                </a-menu>
-            </a-layout-sider>
+            <LayoutSlider></LayoutSlider>
             <a-layout style="padding: 0 24px 24px">
-                <a-breadcrumb style="margin: 16px 0">
-                    <a-breadcrumb-item>Home</a-breadcrumb-item>
-                    <a-breadcrumb-item>List</a-breadcrumb-item>
-                    <a-breadcrumb-item>App</a-breadcrumb-item>
-                </a-breadcrumb>
                 <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-                    Content
+                    <AddTask></AddTask>
                 </a-layout-content>
             </a-layout>
         </a-layout>
@@ -60,10 +15,10 @@
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
 import DashBoardHeader from '@/components/LandingDashBoard/DashBoardHeader.vue'
-const selectedKeys2 = ref<string[]>(['1']);
-const openKeys = ref<string[]>(['sub1']);
+import LayoutSlider from '@/components/LandingDashBoard/LayoutSlider.vue'
+import AddTask from '@/components/LandingDashBoard/AddTask/AddTaskContainer.vue'
+
 </script>
 <style scoped>
 .header {
